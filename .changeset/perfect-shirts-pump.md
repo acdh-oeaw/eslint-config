b71e35721236e1a63161e737e10cd56090c3b4cb
Stefan Probst <stefanprobst@posteo.de>
2023-05-22 11:46:06 +0200

    feat: add react and next.js eslint configs

---
+++ .changeset/perfect-shirts-pump.md
@@ -0,0 +1,6 @@
+---
+"@acdh-oeaw/eslint-config-next": major
+"@acdh-oeaw/eslint-config-react": major
+---
+
+publish shared acdh eslint configs for react and next.js
---
+++ packages/next/config.js
@@ -0,0 +1,55 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+	overrides: [
+		{
+			files: ["./**/*.@(ts|tsx)"],
+			extends: ["plugin:@next/next/core-web-vitals"],
+			rules: {
+				"import/no-default-export": "error",
+			},
+		},
+		{
+			files: ["./**/*.d.ts"],
+			rules: {
+				"import/no-default-export": "off",
+			},
+		},
+		{
+			files: ["./*.config.ts", "./*.config.mjs"],
+			rules: {
+				"import/no-default-export": "off",
+			},
+		},
+		{
+			files: ["./middleware.ts", "./src/middleware.ts"],
+			rules: {
+				"import/no-default-export": "off",
+			},
+		},
+		{
+			files: [
+				"./app/**/default.tsx",
+				"./app/**/error.tsx",
+				"./app/**/global-error.tsx",
+				"./app/**/layout.tsx",
+				"./app/**/loading.tsx",
+				"./app/**/not-found.tsx",
+				"./app/**/page.tsx",
+				"./app/**/template.tsx",
+				"./src/app/**/default.tsx",
+				"./src/app/**/error.tsx",
+				"./src/app/**/global-error.tsx",
+				"./src/app/**/layout.tsx",
+				"./src/app/**/loading.tsx",
+				"./src/app/**/not-found.tsx",
+				"./src/app/**/page.tsx",
+				"./src/app/**/template.tsx",
+			],
+			rules: {
+				"import/no-default-export": "off",
+			},
+		},
+	],
+};
+
+module.exports = config;
