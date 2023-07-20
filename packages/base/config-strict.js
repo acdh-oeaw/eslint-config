88a9360157a2357083c24436d098b1a223be8cae
Stefan Probst <stefanprobst@posteo.de>
2023-07-20 06:54:35 +0200

    chore: bump deps

--- packages/base/config-strict.js
+++ packages/base/config-strict.js
@@ -1,8 +1,6 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
-	extends: [
-		"plugin:@typescript-eslint/strict-type-checked",
-	]
+	extends: ["plugin:@typescript-eslint/strict-type-checked"],
 };
 
 module.exports = config;
