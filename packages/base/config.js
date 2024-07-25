09bce30cfc88cfc9d84c93aa391ed251bbe5f127
Stefan Probst <stefanprobst@posteo.de>
2024-07-25 15:53:40 +0200

    chore: wip [skip ci]

--- packages/base/config.js
+++ packages/base/config.js
@@ -1,126 +1,64 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+/** @typedef {import("eslint").Linter.Config} Config */
 
-import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
+import ts from "typescript-eslint";
+// import importPlugin from "eslint-plugin-import-x";
+import * as regexpPlugin from "eslint-plugin-regexp";
+import importSortPlugin from "eslint-plugin-simple-import-sort";
+import prettier from "eslint-config-prettier";
+import { FlatCompat } from "@eslint/eslintrc";
 import globals from "globals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
+	baseDirectory: import.meta.dirname,
 });
 
-export default [
-	...compat.extends(
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended-type-checked",
-		"plugin:@typescript-eslint/stylistic-type-checked",
-		"plugin:import-x/recommended",
-		"plugin:import-x/typescript",
-		"plugin:regexp/recommended",
-		"prettier",
-	),
+/** @type {Array<Config>} */
+const config = ts.config(
 	{
-		plugins: {
-			"simple-import-sort": simpleImportSort,
-		},
 		linterOptions: {
 			reportUnusedDisableDirectives: true,
 		},
+	},
+	js.configs.recommended,
+	// ...ts.configs.recommendedTypeChecked,
+	...ts.configs.strictTypeChecked,
+	...ts.configs.stylisticTypeChecked,
+	{
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-			ecmaVersion: 2023,
-			sourceType: "commonjs",
+			// globals: {
+			// 	...globals.browser,
+			// 	...globals.node,
+			// },
 			parserOptions: {
-				project: ["./tsconfig.json"],
-			},
-		},
-		settings: {
-			"import-x/internal-regex": "^@/",
-			"import-x/parsers": {
-				"@typescript-eslint/parser": [".ts", ".tsx", ".js", ".mjs", ".cjs", ".mts", ".cts"],
-			},
-			"import-x/resolver": {
-				typescript: {
-					project: ["./tsconfig.json"],
-					alwaysTryTypes: true,
-				},
+				// ecmaVersion: 2023,
+				// sourceType: "module",
+				project: true,
+				// tsconfigRootDir: import.meta.dirname,
 			},
 		},
 		rules: {
 			"consistent-return": "error",
-			eqeqeq: [
-				"error",
-				"always",
-				{
-					null: "ignore",
-				},
-			],
-			"no-console": [
-				"warn",
-				{
-					allow: ["warn", "error"],
-				},
-			],
+			eqeqeq: ["error", "always", { null: "ignore" }],
+			"no-console": ["warn", { allow: ["warn", "error"] }],
 			"no-implicit-coercion": "error",
 			"no-param-reassign": "error",
-			"no-restricted-globals": [
-				"error",
-				{
-					name: "isNaN",
-					message: "Use Number.isNaN instead.",
-				},
-			],
+			"no-restricted-globals": ["error", { name: "isNaN", message: "Use Number.isNaN instead." }],
 			"no-throw-literal": "error",
 			"require-atomic-updates": "error",
-			"@typescript-eslint/array-type": [
-				"error",
-				{
-					default: "generic",
-				},
-			],
+			"@typescript-eslint/array-type": ["error", { default: "generic" }],
 			"@typescript-eslint/consistent-type-exports": [
 				"error",
-				{
-					fixMixedExportsWithInlineTypeSpecifier: true,
-				},
+				{ fixMixedExportsWithInlineTypeSpecifier: true },
 			],
-			"@typescript-eslint/consistent-type-imports": [
-				"error",
-				{
-					fixStyle: "inline-type-imports",
-				},
-			],
-			"@typescript-eslint/no-empty-interface": [
-				"error",
-				{
-					allowSingleExtends: true,
-				},
-			],
+			"@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
+			"@typescript-eslint/no-empty-interface": ["error", { allowSingleExtends: true }],
 			"@typescript-eslint/no-import-type-side-effects": "error",
 			"@typescript-eslint/no-misused-promises": [
 				"error",
-				{
-					checksVoidReturn: {
-						arguments: false,
-						attributes: false,
-					},
-				},
+				{ checksVoidReturn: { arguments: false, attributes: false } },
 			],
-			"@typescript-eslint/no-namespace": [
-				"error",
-				{
-					allowDeclarations: true,
-				},
-			],
+			"@typescript-eslint/no-namespace": ["error", { allowDeclarations: true }],
 			"@typescript-eslint/no-unnecessary-condition": "error",
 			"@typescript-eslint/no-unused-vars": [
 				"error",
@@ -132,23 +70,37 @@ export default [
 			"@typescript-eslint/no-unnecessary-template-expression": "error",
 			"@typescript-eslint/sort-type-constituents": "error",
 			"@typescript-eslint/switch-exhaustiveness-check": "error",
+		},
+	},
+	...compat.extends("plugin:import-x/recommended", "plugin:import-x/typescript"),
+	{
+		settings: {
+			"import-x/internal-regex": "^[@~]/",
+			"import-x/resolver": {
+				typescript: {
+					alwaysTryTypes: true,
+					project: true,
+				},
+			},
+		},
+		rules: {
 			"import-x/first": "error",
 			"import-x/newline-after-import": "error",
 			"import-x/no-anonymous-default-export": "error",
-			"import-x/no-duplicates": [
-				"error",
-				{
-					"prefer-inline": true,
-				},
-			],
+			"import-x/no-duplicates": ["error", { "prefer-inline": true }],
+		},
+	},
+	{
+		plugins: {
+			"simple-import-sort": importSortPlugin,
+		},
+		rules: {
 			"simple-import-sort/imports": "error",
 			"simple-import-sort/exports": "error",
 		},
 	},
-	{
-		files: ["./**/*.cjs"],
-		rules: {
-			"@typescript-eslint/no-var-requires": "off",
-		},
-	},
-];
+	regexpPlugin.configs["flat/recommended"],
+	prettier,
+);
+
+export default config;
