09cf55e9212254a1d645e5af1b3db530580f5ee3
Stefan Probst <stefanprobst@posteo.de>
2024-09-05 19:56:29 +0200

    fix: use new file globs

---
+++ .changeset/brave-fireants-tell.md
@@ -0,0 +1,6 @@
+---
+"@acdh-oeaw/eslint-config-playwright": patch
+"@acdh-oeaw/eslint-config-next": patch
+---
+
+fix file globs
--- packages/playwright/config.js
+++ packages/playwright/config.js
@@ -5,7 +5,7 @@ import ts from "typescript-eslint";
 const config = ts.config({
 	name: "acdh-oeaw/playwright-config",
 	...playwrightPlugin.configs["flat/recommended"],
-	files: ["./e2e/**/*.@(spec|test).@(ts|tsx)"],
+	files: ["e2e/**/*.@(spec|test).@(ts|tsx)"],
 });
 
 export default config;
