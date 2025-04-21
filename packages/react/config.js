d2b0326d3cd37f9e29f16bf8fac1c30c6a934226
Stefan Probst <stefanprobst@posteo.de>
2025-04-21 23:18:11 +0200

    chore: update config for react compiler

--- packages/react/config.js
+++ packages/react/config.js
@@ -2,7 +2,6 @@ import reactXPlugin from "@eslint-react/eslint-plugin";
 import prettier from "eslint-config-prettier";
 import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import reactPlugin from "eslint-plugin-react";
-import reactCompilerPlugin from "eslint-plugin-react-compiler";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
 // import globals from "globals";
 import ts from "typescript-eslint";
@@ -10,15 +9,12 @@ import ts from "typescript-eslint";
 const config = ts.config(
 	reactPlugin.configs.flat.recommended,
 	reactPlugin.configs.flat["jsx-runtime"],
+	reactHooksPlugin.configs.recommended,
 	// TODO: enable preset and de-duplicate rules from eslint-plugin-react
 	// reactXPlugin.configs["recommended-type-checked"],
 	jsxA11yPlugin.flatConfigs.recommended,
 	{
 		name: "acdh-oeaw/react-config",
-		plugins: {
-			"react-compiler": reactCompilerPlugin,
-			"react-hooks": reactHooksPlugin,
-		},
 		settings: {
 			react: {
 				version: "detect",
@@ -27,8 +23,6 @@ const config = ts.config(
 			linkComponents: [{ name: "Link" }, { name: "NavLink" }],
 		},
 		rules: {
-			...reactHooksPlugin.configs.recommended.rules,
-
 			"jsx-a11y/anchor-is-valid": ["error", { components: ["Link"] }],
 			"jsx-a11y/no-autofocus": ["error", { ignoreNonDOM: true }],
 			"jsx-a11y/no-redundant-roles": [
@@ -49,8 +43,6 @@ const config = ts.config(
 			// "react/jsx-sort-props": ["error", { reservedFirst: true }],
 			"react/no-unstable-nested-components": "error",
 			"react/prop-types": "off",
-
-			"react-compiler/react-compiler": "error",
 		},
 	},
 	{
