4ead6dddb832ace6f02cfa7601d7cf1485e51fd5
kkukelka <17953978+kkukelka@users.noreply.github.com>
2025-09-08 19:44:21 +0200

    fix: change 'extends' property values to arrays

--- packages/mdx/config.js
+++ packages/mdx/config.js
@@ -4,13 +4,13 @@ import * as mdx from "eslint-plugin-mdx";
 const config = defineConfig(
 	{
 		name: "acdh-oeaw/mdx-config",
-		extends: mdx.flat,
+		extends: [mdx.flat],
 		/** Lint code blocks. */
 		processor: mdx.createRemarkProcessor({ lintCodeBlocks: true }),
 	},
 	{
 		name: "acdh-oeaw/mdx-config/code-blocks",
-		extends: mdx.flatCodeBlocks,
+		extends: [mdx.flatCodeBlocks],
 	},
 );
 
