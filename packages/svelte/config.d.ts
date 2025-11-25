324ff71f227ffd3ad6a621b5134839f5325f4b46
stefanprobst <stefanprobst@posteo.de>
2025-11-25 07:58:17 +0100

    feat: add svelte config preset (#111)

---
+++ packages/svelte/config.d.ts
@@ -0,0 +1,5 @@
+import type { ConfigWithExtends } from "@eslint/config-helpers";
+
+declare const config: ConfigWithExtends;
+
+export default config;
---
+++ packages/svelte/config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier";
+import sveltePlugin from "eslint-plugin-svelte";
+import globals from "globals";
+import ts from "typescript-eslint";
+
+const config = defineConfig(
+	sveltePlugin.configs["flat/recommended"],
+	{
+		name: "acdh-oeaw/svelte-config",
+		files: ["**/*.svelte", "**/*.svelte.ts"],
+		languageOptions: {
+			ecmaVersion: 2023,
+			globals: {
+				...globals.es2023,
+				...globals["shared-node-browser"],
+			},
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: [".svelte"],
+				parser: ts.parser,
+			},
+		},
+	},
+	{
+		name: "@acdh-oeaw/svelte-config/node-globals",
+		files: ["svelte.config.*", "config/env.config.*", "scripts/**"],
+		languageOptions: {
+			globals: {
+				...globals.nodeBuiltin,
+			},
+		},
+	},
+	{
+		name: "@acdh-oeaw/svelte-config/browser-globals",
+		ignores: ["svelte.config.*", "config/env.config.*", "scripts/**"],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+		},
+	},
+	prettier,
+);
+
+export default config;
